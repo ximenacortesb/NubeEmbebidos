f852429eaed9b02bcca65002c1246b5bd98d23f6
Ximena Cortes <ximenacortesb@ciencias.unam.mx>
2022-09-28 16:07:42 -0500

    Agrego sketches SPIFFS y wifi

---
+++ SPIFFS_read_test_IP/wifi_test.ino
@@ -0,0 +1,36 @@
+#include <WiFi.h>
+
+const char* ssid     = "ssid";
+const char* password = "password";
+
+
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
